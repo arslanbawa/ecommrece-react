090e73f121f58a4e86964e86fae308c26fe6eb03
arslan <arslan.ptf@gmail.com>
2021-09-23 18:16:26 +0500

    adding grid component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Grid from '../../components/grid/Grid'
 
 export default function index() {
     return (
         <div>
                 <h1>Home</h1>
+                <Grid name={"jewelery"} />
         </div>
     )
 }
